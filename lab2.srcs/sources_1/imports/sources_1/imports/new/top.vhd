c0546e094eec04e46445116bf913b5e7230bf6de
jaoff15 <jaoff15@student.sdu.dk>
2019-02-20 15:20:07 +0100

    led_matrix module added

--- lab2.srcs/sources_1/imports/sources_1/imports/new/top.vhd
+++ lab2.srcs/sources_1/imports/sources_1/imports/new/top.vhd
@@ -15,35 +15,19 @@ entity top is
 end top;
 
 architecture Behavioral of top is
-
-    -- Define interface with row
-    component m_row is
+    component led_matrix is
     Port (  
-            counter     : in  unsigned(7 downto 0)          := "00000000";
-            red         : in  std_logic_vector(63 downto 0) := x"0000000000000000"; 
-            green       : in  std_logic_vector(63 downto 0) := x"0000000000000000";
-            blue        : in  std_logic_vector(63 downto 0) := x"0000000000000000";
-            red_pwm     : out std_logic_vector(7  downto 0) := "00000000";
-            green_pwm   : out std_logic_vector(7  downto 0) := "00000000";
-            blue_pwm    : out std_logic_vector(7  downto 0) := "00000000"
+           pwm_clock    : in STD_LOGIC;
+           row_clock    : in STD_LOGIC;
+           row          : out STD_LOGIC_VECTOR (7 downto 0);
+           red          : out STD_LOGIC_VECTOR (7 downto 0);
+           green        : out STD_LOGIC_VECTOR (7 downto 0);
+           blue         : out STD_LOGIC_VECTOR (7 downto 0)
            );
     end component;
-    
+  -- Clock prescaler
+  signal prescaler            : unsigned(31 downto 0) := x"00000000";
 
-    -- Clock prescaler
-    signal prescaler       : unsigned(31 downto 0) := x"00000000";
-    
-    -- Prescaled clock counter
-    signal counter         : unsigned(7 downto 0)  := "00000000";
-    
-   
---    -- Shift register to control which row to display
-    signal row_shift_register : unsigned (7 downto 0) := "11111110";
-    
---    -- Color intensities
-    signal red_intensity    : std_logic_vector(63 downto 0) := x"0000000000000000"; 
-    signal green_intensity  : std_logic_vector(63 downto 0) := x"0000000000000000";
-    signal blue_intensity   : std_logic_vector(63 downto 0) := x"0000000000000000";
 begin
 
 prescaling_process: process (clk_8ns)
@@ -54,91 +38,15 @@ begin
 end process;
 
 
-counter_process: process (prescaler)
-begin
--- 125MHz / 2*2^13 = 7629kHz
-   if rising_edge(prescaler(12)) then
-        counter <= counter + 1;
-   end if;
-end process;
-
-
-row_process: process (prescaler)
-begin
-    if rising_edge(prescaler(11)) then
-        -- Shift bits one bit to the left
-        row_shift_register <= row_shift_register rol 1;
-    end if;
-end process;
-
-
-
-color_picker_process: process(row_shift_register)
-begin
---     FF = 100%
---     80 = 50%
---     00 = 0%    
-
---     Red
-    case row_shift_register is
-      when "11111110" => red_intensity <=    x"0000000000000000";
-      when "11111101" => red_intensity <=    x"0000000000000000";
-      when "11111011" => red_intensity <=    x"000000FF00000000";
-      when "11110111" => red_intensity <=    x"0000000000000000";
-      when "11101111" => red_intensity <=    x"0000000000000000";
-      when "11011111" => red_intensity <=    x"0000000000000000";
-      when "10111111" => red_intensity <=    x"0000000000000000";
-      when "01111111" => red_intensity <=    x"0000000000000000";
-      -- Others
-      when others     => red_intensity <=    x"0000000000000000";
-   end case;
-   
-   -- Green
-   case row_shift_register is
-      when "11111110" => green_intensity <=    x"0000000000000000";
-      when "11111101" => green_intensity <=    x"0000000000000000";
-      when "11111011" => green_intensity <=    x"00000000000000FF";
-      when "11110111" => green_intensity <=    x"0000000000000000";
-      when "11101111" => green_intensity <=    x"0000000000000000";
-      when "11011111" => green_intensity <=    x"0000000000000000";
-      when "10111111" => green_intensity <=    x"0000000000000000";
-      when "01111111" => green_intensity <=    x"0000000000000000";
-      -- Others
-      when others     => green_intensity <=    x"0000000000000000";
-   end case;
-   
-   -- Blue
-   case row_shift_register is
-      when "11111110" => blue_intensity <=    x"0000000000000000";
-      when "11111101" => blue_intensity <=    x"0000000000000000";
-      when "11111011" => blue_intensity <=    x"0000000000000000";
-      when "11110111" => blue_intensity <=    x"0000000000000000";
-      when "11101111" => blue_intensity <=    x"0000000000000000";
-      when "11011111" => blue_intensity <=    x"FF00000000000000";
-      when "10111111" => blue_intensity <=    x"0000000000000000";
-      when "01111111" => blue_intensity <=    x"0000000000000000";
-      -- Others
-      when others     => blue_intensity <=    x"0000000000000000";
-   end case;
-   
-   
-   row <= std_logic_vector(row_shift_register);
-end process;
-
-
-
-
-
-
-row0:m_row
+led_matrix0:led_matrix
 port map(
-    counter     => counter,
-    red         => red_intensity,
-    green       => green_intensity,
-    blue        => blue_intensity,
-    red_pwm     => red,
-    green_pwm   => green,                                                                                                                                                                                  
-    blue_pwm    => blue
-    );
+    pwm_clock    => prescaler(12),
+    row_clock    => prescaler(11),
+    row          => row,
+    red          => red,
+    green        => green,
+    blue         => blue
+);
+
 
 end Behavioral;
---
+++ lab2.srcs/sources_1/new/led_matrix.vhd
@@ -0,0 +1,132 @@
+
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+
+entity led_matrix is
+    Port ( pwm_clock    : in std_logic;
+           row_clock    : in std_logic;
+           row          : out std_logic_vector (7 downto 0);
+           red          : out std_logic_vector (7 downto 0);
+           green        : out std_logic_vector (7 downto 0);
+           blue         : out std_logic_vector (7 downto 0)
+           );
+end led_matrix;
+
+architecture Behavioral of led_matrix is
+     -- Define interface with row
+    component m_row is
+    Port (  
+            counter     : in  unsigned(7 downto 0)          := "00000000";
+            red         : in  std_logic_vector(63 downto 0) := x"0000000000000000"; 
+            green       : in  std_logic_vector(63 downto 0) := x"0000000000000000";
+            blue        : in  std_logic_vector(63 downto 0) := x"0000000000000000";
+            red_pwm     : out std_logic_vector(7  downto 0) := "00000000";
+            green_pwm   : out std_logic_vector(7  downto 0) := "00000000";
+            blue_pwm    : out std_logic_vector(7  downto 0) := "00000000"
+           );
+    end component;
+    
+    
+    -- Define interface with simple_memory
+    component simple_memory is
+    Port(
+            row_addr       : in STD_LOGIC_VECTOR (2 downto 0);
+            data_red       : out STD_LOGIC_VECTOR (63 downto 0);
+            data_green     : out STD_LOGIC_VECTOR (63 downto 0);
+            data_blue      : out STD_LOGIC_VECTOR (63 downto 0)
+    );
+    end component;
+    
+    -- Prescaled clock counter
+    signal counter              : unsigned(7 downto 0)  := "00000000";
+    
+    -- Row counter
+    signal row_counter          : unsigned(2 downto 0) := "000";
+    
+    
+    -- Define states
+    constant STATE_SHIFT_ROW    : std_logic := '0';
+    constant STATE_READY        : std_logic := '1'; 
+    
+    -- State
+    signal state                : std_logic := STATE_SHIFT_ROW;
+
+    -- Color threshold signals
+    signal red_sig              : std_logic_vector(63 downto 0) := x"0000000000000000";
+    signal green_sig            : std_logic_vector(63 downto 0) := x"0000000000000000";
+    signal blue_sig             : std_logic_vector(63 downto 0) := x"0000000000000000";
+    
+    -- Color pwm signals
+    signal red_pwm_sig          : std_logic_vector(7 downto 0) := "00000000";
+    signal green_pwm_sig        : std_logic_vector(7 downto 0) := "00000000";
+    signal blue_pwm_sig         : std_logic_vector(7 downto 0) := "00000000";
+begin
+
+
+counter_process: process (pwm_clock)
+begin
+-- 125MHz / 2*2^13 = 7629kHz
+   if rising_edge(pwm_clock) then
+        counter <= counter + 1;
+   end if;
+end process;
+
+
+row_counter_process: process (row_clock)
+begin
+   if rising_edge(row_clock) then
+       -- State shift row
+       if state = STATE_SHIFT_ROW then
+            red <= red_pwm_sig;
+            green <= green_pwm_sig;
+            blue <= blue_pwm_sig;
+            
+           case row_counter is
+              when "000"  => row <= "01111111";
+              when "001"  => row <= "10111111";
+              when "010"  => row <= "11011111";
+              when "011"  => row <= "11101111";
+              when "100"  => row <= "11110111";
+              when "101"  => row <= "11111011";
+              when "110"  => row <= "11111101";
+              when "111"  => row <= "11111110";
+              when others => row <= "11111111";
+           end case;
+ 
+            state <= STATE_READY;
+            
+       -- State ready
+       elsif state = STATE_READY then
+            row_counter <= row_counter + 1;
+            state <= STATE_SHIFT_ROW;
+       end if;
+   end if;
+end process;
+
+
+
+
+row0:m_row
+port map(
+    counter     => counter,
+    red         => red_sig,
+    green       => green_sig,
+    blue        => blue_sig,
+    red_pwm     => red_pwm_sig,
+    green_pwm   => green_pwm_sig,                                                                                                                                                                                  
+    blue_pwm    => blue_pwm_sig
+);
+
+
+simple_memory0: simple_memory
+port map(
+    row_addr       => std_logic_vector(row_counter),
+    data_red       => red_sig,
+    data_green     => green_sig,
+    data_blue      => blue_sig
+);
+
+end Behavioral;
